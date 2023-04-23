0d2814321d94ccf696ac26371a9cb82161b84923
Benjamin Wagner <bnm.wag@gmail.com>
2023-04-24 00:07:30 +0200

    replace handle... router functions with next/link

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -14,30 +14,19 @@ const Avatar: React.FunctionComponent<IAvatarProps> = ({
 	isLarge,
 	hasBorder,
 }) => {
-	const router = useRouter();
 	const { data: fetchedUser } = useUser(userId);
-	const onClick = useCallback(
-		(event: any) => {
-			event.stopPropagation();
-
-			const url = `/users/${userId}`;
-
-			router.push(url);
-		},
-		[router, userId]
-	);
 
 	return (
 		<div
 			className={`
-        ${hasBorder ? 'border-4 border-black' : ''}
-        ${isLarge ? 'h-32' : 'h-12'}
-        ${isLarge ? 'w-32' : 'w-12'}
-        rounded-full 
-        hover:opacity-90
-        transition
-        cursor-pointer
-    `}
+       			${hasBorder ? 'border-4 border-black' : ''}
+       			${isLarge ? 'h-32' : 'h-12'}
+       			${isLarge ? 'w-32' : 'w-12'}
+       			rounded-full 
+       			hover:opacity-90
+       			transition
+       			cursor-pointer
+    		`}
 		>
 			<Image
 				width={isLarge ? 128 : 48}
@@ -48,9 +37,8 @@ const Avatar: React.FunctionComponent<IAvatarProps> = ({
 					width: '100%',
 					height: '100%',
 				}}
-				alt='avatr'
-				onClick={onClick}
-				src={fetchedUser?.s || '/images/placeholder.gif'}
+				alt={`${fetchedUser?.name}'s avatar image`}
+				src={fetchedUser?.avatar || '/images/placeholder.gif'}
 			/>
 		</div>
 	);
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -4,22 +4,21 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 import Button from '../Button';
 import useLoginModal from '@/hooks/useLoginModal';
 import Avatar from '../Avatar';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 interface INavBarProps {}
 
 const NavBar: React.FC<INavBarProps> = (props) => {
+	const router = useRouter();
 	const { data: currentUser } = useCurrentUser();
 	const loginModal = useLoginModal();
-
-	const handleLoginClick = () => {
-		loginModal.onOpen();
-	};
 
 	return (
 		<div className='fixed top-0 left-0 w-screen h-[75px] shadow-md bg-white z-50'>
 			<div className='flex items-center justify-between h-full px-36'>
 				<div className='flex items-center gap-36 h-full'>
-					<div className='flex items-center'>
+					<Link className='flex items-center cursor-pointer' href='/'>
 						<div className='w-10 h-10 mr-2'>
 							<Image
 								src='/district-logo.svg'
@@ -30,15 +29,15 @@ const NavBar: React.FC<INavBarProps> = (props) => {
 							/>
 						</div>
 						<div className='text-xl font-medium'>District</div>
-					</div>
+					</Link>
 
 					<SearchBar />
 				</div>
 				<div className='flex items-center gap-36 h-full'>
 					{currentUser ? (
-						<div
+						<Link
 							className='flex items-center gap-4'
-							onClick={handleLoginClick}
+							href={`/users/${currentUser?.id}`}
 						>
 							<div className='flex flex-col items-end'>
 								<div className='text-sm font-bold'>
@@ -46,13 +45,13 @@ const NavBar: React.FC<INavBarProps> = (props) => {
 								</div>
 								{/* <div className='text-xs font-light'>Admin</div> */}
 							</div>
-							<Avatar userId={currentUser?._id} />
-						</div>
+							<Avatar userId={currentUser?.id} />
+						</Link>
 					) : (
 						<Button
 							label='Login'
 							secondary
-							onClick={handleLoginClick}
+							onClick={() => loginModal.onOpen()}
 						/>
 					)}
 				</div>
